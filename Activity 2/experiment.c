813326a41ec12aa17640e64b970f3ae690b2e595
lalayn <alaynmontezo@gmail.com>
2018-06-16 15:51:16 +0800

    Update experiment.c

--- Activity 2/experiment.c
+++ Activity 2/experiment.c
@@ -10,3 +10,51 @@ int main(void)
 
 }
 
+
+void sortRecords(int display){
+	int i;
+	
+	switch(display){
+    	case 1:
+    	printf("\n\n*****************************************************************************\n\n");
+    	printf("                          GROUP EXPERIMENT RECORDS                               \n\n");
+    	printf("*****************************************************************************\n\n");   
+ 		for(i = 0; i < 9; i++){
+ 			printf("GROUP NUMBER: %d \tEXPERIMENT TITLE: %s\n", records[i].group, records[i].title);
+		}
+		break;	
+		
+		case 2:
+    	printf("\n\n*****************************************************************************\n\n");
+    	printf("                          BIOLOGY EXPERIMENT RECORDS                               \n\n");
+    	printf("*****************************************************************************\n\n");   
+ 		for(i = 0; i < 9; i++){
+ 			if(strcmp(records[i].field, "Biology") == 0){
+ 				printf("GROUP NUMBER: %d \tEXPERIMENT TITLE: %s\n", records[i].group, records[i].title);
+ 			}
+		}
+		break;	
+		
+		case 3:
+    	printf("\n\n*****************************************************************************\n\n");
+    	printf("                          CHEMISTRY EXPERIMENT RECORDS                               \n\n");
+    	printf("*****************************************************************************\n\n");   
+ 		for(i = 0; i < 9; i++){
+ 			if(strcmp(records[i].field, "Chemistry") == 0){
+ 				printf("GROUP NUMBER: %d \tEXPERIMENT TITLE: %s\n", records[i].group, records[i].title);
+ 			}
+		}
+		break;	
+		
+		case 4:
+    	printf("\n\n*****************************************************************************\n\n");
+    	printf("                          PHYSICS EXPERIMENT RECORDS                               \n\n");
+    	printf("*****************************************************************************\n\n");   
+ 		for(i = 0; i < 9; i++){
+ 			if(strcmp(records[i].field, "Physics") == 0){
+ 				printf("GROUP NUMBER: %d \tEXPERIMENT TITLE: %s\n", records[i].group, records[i].title);
+ 			}
+		}
+		break;	
+	}
+}
